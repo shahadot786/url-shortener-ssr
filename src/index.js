3453aefd9205c771dce243f8e6e8d54fd21d32a6
shahadot786 <shrhossain786@gmail.com>
2025-12-25 00:56:14 +0600

    feat: add EJS view engine support and create home view template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import router from "./routes/url.route.js";
 import connectDB from "./config/dbConnection.js";
 
@@ -6,12 +7,16 @@ const app = express();
 const PORT = 8000;
 const DBUrl = "mongodb://127.0.0.1:27017/short-url";
 
-app.use(express.json());
-
 //connect db
 connectDB(DBUrl)
   .then(() => console.log("Database connected successfully."))
   .catch(() => console.log("DB Connection error."));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
 
